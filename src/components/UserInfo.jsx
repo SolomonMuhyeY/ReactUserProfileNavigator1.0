a72def5ca0ddd424562d8860cafbbcd3fa6bef1a
SolomonMuhye <solomon.muhye.wd@gmail.com>
2023-07-15 02:36:43 +0300

    adding a property called desctiption

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -14,47 +14,63 @@ export const clinet = [
     btnMsg: "Click to view profile",
     img: image1,
     index: 1,
+    description:
+      "Abel Mamo is one of our best client or user of our service in the company.He has been here since the very first time he really is one of our main client. And also the one with best attitude towards our company.",
   },
   {
     name: "Danny Belete",
     btnMsg: "Click to view profile",
     img: image2,
     index: 2,
+    description:
+      "Danny Belete is one of our best client or user of our service in the company.He has been here since the very first time he really is one of our main client. And also the one with best attitude towards our company.",
   },
   {
     name: "Meaza Ayalew",
     btnMsg: "Click to view profile",
     img: image3,
     index: 3,
+    description:
+      "Meaza Ayalew is one of our best client or user of our service in the company.She has been here since the very first time he really is one of our main client. And also the one with best attitude towards our company.",
   },
   {
     name: "Kedir Arega",
     btnMsg: "Click to view profile",
     img: image4,
     index: 4,
+    description:
+      "Kedir Arega is one of our best client or user of our service in the company.He has been here since the very first time he really is one of our main client. And also the one with best attitude towards our company.",
   },
   {
     name: "Lemma Bekele",
     btnMsg: "Click to view profile",
     img: image5,
     index: 5,
+    description:
+      "Lema Bekele is one of our best client or user of our service in the company.He has been here since the very first time he really is one of our main client. And also the one with best attitude towards our company.",
   },
   {
     name: "Selam Alemu",
     btnMsg: "Click to view profile",
     img: image6,
     index: 6,
+    description:
+      "Selam Alemu is one of our best client or user of our service in the company.She has been here since the very first time he really is one of our main client. And also the one with best attitude towards our company.",
   },
   {
     name: "Alemu Abera",
     btnMsg: "Click to view profile",
     img: image7,
     index: 7,
+    description:
+      "Alemu Abera is one of our best client or user of our service in the company.He has been here since the very first time he really is one of our main client. And also the one with best attitude towards our company.",
   },
   {
     name: "Senayit Bekele",
     btnMsg: "Click to view profile",
     img: image8,
     index: 8,
+    description:
+      "Senayit Bekele is one of our best client or user of our service in the company.She has been here since the very first time he really is one of our main client. And also the one with best attitude towards our company.",
   },
 ];
